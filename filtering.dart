c4eb36aaf820a1478d27d99640a3f434122742b6
Jimmy Anders <152792663+gimmyanders1990@users.noreply.github.com>
2023-12-03 16:17:19 +0200

    Update filtering.dart

--- filtering.dart
+++ filtering.dart
@@ -6,6 +6,17 @@ export 'src/pylons_wallet.dart';
 export 'src/types/cookbook.dart';
 export 'src/types/execution.dart';
 export 'src/types/item.dart';
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    PylonsWallet.instance.exists().then((value) {
+      log('WALLET Existence $value');
+    });
+  }
+
+  
 export 'src/types/profile.dart';
 export 'src/types/recipe.dart';
 export 'src/features/helper/dec_string.dart';
